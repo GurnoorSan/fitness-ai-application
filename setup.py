c13e19ccf1f896cc6fe4103725df980f39f0fade
skuxxxxx <105649765+skuxxxxx@users.noreply.github.com>
2025-01-03 21:31:28 +1300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="fitbot",
+    version="0.0.0",
+    author="Gurnoor Sandhu",
+    author_email="sandhugurnoor256@gmail.com",
+    packages=find_packages(),
+)
